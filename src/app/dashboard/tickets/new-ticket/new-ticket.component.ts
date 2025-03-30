091557c1abfc58ca57d3d0848c882bcc47d3529a
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-03-30 09:13:07 -0400

    control component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from "../../../shared/control/control.component";
+
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
+  imports: [ButtonComponent, ControlComponent]
 })
 export class NewTicketComponent {
 
