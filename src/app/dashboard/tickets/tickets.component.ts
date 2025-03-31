69b125b7c5d0b8f1eaf200819994b5e505263c74
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-03-31 11:31:26 -0400

    onCloseTicket

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -5,11 +5,11 @@ import { Ticket } from './ticket.model';
 import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
-    selector: 'app-tickets',
-    standalone: true,
-    templateUrl: './tickets.component.html',
-    styleUrl: './tickets.component.css',
-    imports: [NewTicketComponent, TicketComponent]
+  selector: 'app-tickets',
+  standalone: true,
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.css',
+  imports: [NewTicketComponent, TicketComponent]
 })
 export class TicketsComponent {
   tickets: Ticket[] = [];
@@ -23,5 +23,14 @@ export class TicketsComponent {
     }
 
     this.tickets.push(ticket);
+  }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' }
+      }
+      return ticket;
+    });
   }
 }
