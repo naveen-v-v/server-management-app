6cfe44c3a2c8562bb4711c9fc5f8ceb37871909a
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-03-31 10:34:18 -0400

    HTML form template variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
     selector: 'app-new-ticket',
     standalone: true,
     templateUrl: './new-ticket.component.html',
     styleUrl: './new-ticket.component.css',
-    imports: [ButtonComponent, ControlComponent, FormsModule]
+    imports: [ButtonComponent, ControlComponent, FormsModule],
 })
 export class NewTicketComponent {
-    onSubmit() {
-        console.log('SUBMITTED!');
+    onSubmit(title: string, ticketText: String) {
+        console.log(title);
+        console.log(ticketText);
     }
 }
