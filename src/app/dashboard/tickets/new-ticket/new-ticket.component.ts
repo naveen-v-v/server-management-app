c96e859821e279ec8b9954214b108c8cbcdfa185
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-03-31 11:05:32 -0400

    effect function

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -5,30 +5,30 @@ import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
-    selector: 'app-new-ticket',
-    standalone: true,
-    templateUrl: './new-ticket.component.html',
-    styleUrl: './new-ticket.component.css',
-    imports: [ButtonComponent, ControlComponent, FormsModule],
+  selector: 'app-new-ticket',
+  standalone: true,
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+  imports: [ButtonComponent, ControlComponent, FormsModule],
 })
 export class NewTicketComponent implements OnInit, AfterViewInit {
-    @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-    // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
-    ngOnInit() {
-        console.log('ONINIT');
-        console.log(this.form?.nativeElement);
-    }
+  ngOnInit() {
+    console.log('ONINIT');
+    console.log(this.form?.nativeElement);
+  }
 
-    ngAfterViewInit() {
-        console.log('AFTER VIEW INIT');
-        console.log(this.form?.nativeElement);
-    }
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
+  }
 
-    onSubmit(title: string, ticketText: String) {
-        console.log(title);
-        console.log(ticketText);
+  onSubmit(title: string, ticketText: String) {
+    console.log(title);
+    console.log(ticketText);
 
-        this.form?.nativeElement.reset();
-    }
+    this.form?.nativeElement.reset();
+  }
 }
