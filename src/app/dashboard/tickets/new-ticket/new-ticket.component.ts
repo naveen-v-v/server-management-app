f8957f7b84aa2a1b5acb41c12816ee00ac78b919
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-03-31 10:28:01 -0400

    form submission

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
@@ -8,8 +9,10 @@ import { ControlComponent } from "../../../shared/control/control.component";
     standalone: true,
     templateUrl: './new-ticket.component.html',
     styleUrl: './new-ticket.component.css',
-    imports: [ButtonComponent, ControlComponent]
+    imports: [ButtonComponent, ControlComponent, FormsModule]
 })
 export class NewTicketComponent {
-
+    onSubmit() {
+        console.log('SUBMITTED!');
+    }
 }
